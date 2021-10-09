0cc7d86e2c0fdba8bdfa0633d83e6a65f64e2059
LLUVV <jimbo21xc30@gmail.com>
2021-10-09 16:43:19 +0800

    update tsv_to_yml

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -1,14 +1,25 @@
+# frozen_string_literal: true
+
 # Module that can be included (mixin) to take and output TSV data
 module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
-
+    @data = []
+    lines = tsv.split("\n").map { |str| str.split("\t")}
+    keys = lines[0]
+    (1...lines.length).map do |index1|
+      record = {}
+      keys.each_index do |index2|
+        record[keys[index2]] = lines[index1][index2]
+      end
+      @data.push(record)
+    end
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
-
+    
   end
 end
